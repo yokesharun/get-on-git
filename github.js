7ddff988a2b84122b0681dd1fba943ad7568f350
ayokesh <ayokesh.cse@gmail.com>
2015-09-08 01:27:42 +0530

    js issue fixed

--- github.js
+++ github.js
@@ -9,7 +9,8 @@ Template.home.events({
   'submit form': function(event){
     event.preventDefault();
     var search_value = $('#search_value').val();
-
+    if(search_value != "")
+    {
     $.ajax({ 
     type: 'GET', 
     url: 'https://api.github.com/search/repositories', 
@@ -18,11 +19,13 @@ Template.home.events({
     success: function (data) { 
       console.log(data.items);
         $.each(data.items, function(index, element) {
-            console.log(element);
+            $('#collection_list').empty();
             $('#collection_list').append('<li class="collection-item avatar"><img src="'+ element.owner.avatar_url +'" class="circle"><span class="title">Username : <em><a href="'+ element.owner.html_url +'">'+ element.owner.login+'</a></em></span><p>Repo Link : <a href="'+ element.html_url+'">'+ element.html_url+'</a> <br><span>Clone Url :</span> <pre>'+ element.clone_url +'</pre></p></li>');
         });
     }
     });
+  }
+
   }
 });
 
