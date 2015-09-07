81196f241275aaa9c52ec71e52dc352b6974ab08
ayokesh <ayokesh.cse@gmail.com>
2015-09-07 23:13:26 +0530

    initial commit

---
+++ git_app.js
@@ -0,0 +1,15 @@
+if (Meteor.isClient) {
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+Router.map(function () {
+
+	// Home page
+	this.route('home', {
+		name: 'home',
+		path: '/',
+		template: 'views/home'
+	});
+
+});
+
