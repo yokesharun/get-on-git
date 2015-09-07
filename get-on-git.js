b364a95cf935e0c3e65ee8d47cab592e21fe4a68
ayokesh <ayokesh.cse@gmail.com>
2015-09-07 23:35:41 +0530

    meteor initialed

--- get-on-git.js
+++ get-on-git.js
@@ -1,4 +1,12 @@
 if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
 
   Template.hello.events({
     'click button': function () {
